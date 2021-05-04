26b7e04491359833cc7324208976aa2af1856bb7
saurav2811 <saurav.sri2811@gmail.com>
2021-05-05 02:48:17 +0530

    made middleware to protect route so that a logged in user can only use crud functionality

--- routers/auth.js
+++ routers/auth.js
@@ -1,11 +1,12 @@
 const express=require('express')
+const {protect}= require('../middlewares/auth')
 const{getAllUser,register,GetWithInRadius,getspecificuser,updateuser,deleteuser,login}=require('../controllers/auth')
 //initialise router
 const router=express.Router()
-router.route('/').get(getAllUser)
-router.route('/:id').get(getspecificuser).put(updateuser).delete(deleteuser)
+router.route('/').get(protect,getAllUser)
+router.route('/:id').get(protect,getspecificuser).put(protect,updateuser).delete(protect,deleteuser)
 router.route('/register').post(register)
 router.route('/login').post(login)
-router.route('/radius/:zipcode').get(GetWithInRadius)
+router.route('/radius/:zipcode').get(protect,GetWithInRadius)
 
 module.exports=router
